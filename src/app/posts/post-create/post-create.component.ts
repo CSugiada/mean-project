ddd566a5b3b44888a2281008b79e081e5f54150d
CSugiada <40233005+CSugiada@users.noreply.github.com>
2018-11-15 09:52:55 +1100

    Initial Commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Output } from '../../../../node_modules/@angular/core';
+import { Post } from '../post.model';
+import { NgForm } from '../../../../node_modules/@angular/forms';
+
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+    enteredTitle = '';
+    enteredContent = '';
+    @Output() postCreated = new EventEmitter<Post>();
+    
+    onAddPost(form: NgForm){
+        if (form.invalid){
+            return;
+        }
+        const post: Post = {
+            title: form.value.title,
+            content: form.value.content
+        };
+        this.postCreated.emit(post);
+    }
+}
